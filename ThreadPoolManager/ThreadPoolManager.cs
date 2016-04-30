bbff995d688171f8c63da0b21a879fa1eb634186
Brian L <bariuminc@gmail.com>
2016-04-29 17:34:47 -0700

    First push to NuGet.
    Contains extra functionality for ThreadPools.

---
+++ ThreadPoolManager/ThreadPoolManager.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+
+namespace ThreadPoolHelper
+{
+    /// <summary>
+    /// Must use with the <see cref="ThreadPoolObject"/> class.
+    /// </summary>
+    public static class ThreadPoolManager
+    {
+        /// <summary>
+        /// Halts the main thread until all the queued threads have completed.
+        /// </summary>
+        /// <param name="totalQueuedThreads">Total number of threads that were queued.</param>
+        /// <param name="waitTime">How often to check if the threads have completed in seconds. Defaults to 1 second.</param>
+        public static void WaitForThreads(int totalQueuedThreads, double waitTime = 1)
+        {
+            while(totalQueuedThreads <= ThreadPoolObject.Counter)
+            {
+                var counter = ThreadPoolObject.Counter;
+                Thread.Sleep(Convert.ToInt32(waitTime * 1000));
+            }
+        }
+
+        /// <summary>
+        /// Halts the main thread until all the queued threads have completed.
+        /// </summary>
+        /// <param name="collection">Collection that reflects the amount of threads that are queued.</param>
+        /// <param name="waitTime">How often to check if the threads have completed in seconds. Defaults to 1 second.</param>
+        public static void WaitForThreads<T>(IEnumerable<T> collection, double waitTime = 1)
+        {
+            var totalCount = collection.Count();
+            while (totalCount <= ThreadPoolObject.Counter)
+            {
+                var counter = ThreadPoolObject.Counter;
+                Thread.Sleep(Convert.ToInt32(waitTime * 1000));
+            }
+        }
+    }
+}
---
+++ ThreadPoolManager/ThreadPoolObject.cs
@@ -0,0 +1,30 @@
+﻿using System.Threading;
+
+namespace ThreadPoolHelper
+{
+    /// <summary>
+    /// Class used to wrap an object used with the <see cref="ThreadPool"/> class.
+    /// <para>Pass this object as the second parameter in <see cref="ThreadPool.QueueUserWorkItem(WaitCallback, object)"/>.</para>
+    /// Within the <see cref="WaitCallback"/> function cast the object to <see cref="ThreadPoolObject"/>.
+    /// </summary>
+    public class ThreadPoolObject
+    {
+        /// <summary>
+        /// Counter used to display the amount of threads that have completed their function.
+        /// </summary>
+        public static int Counter = 0;
+
+        /// <summary>
+        /// Wrapper for the main functional object used in the <see cref="ThreadPool.QueueUserWorkItem(WaitCallback, object)"/>.
+        /// </summary>
+        public object ThreadObject { get; set; }
+
+        /// <summary>
+        /// Call at the end of each <see cref="ThreadPool.QueueUserWorkItem(WaitCallback, object)"/> function.
+        /// </summary>
+        public void IncrementCounter()
+        {
+            Interlocked.Increment(ref Counter);
+        }
+    }
+}
